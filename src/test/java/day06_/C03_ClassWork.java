74ec1e09ab946a6b307ceea0f68cea9301e3cd6f
Yunus Poyraz <yunuspoyraz710@gmail.com>
2022-12-31 17:42:38 +0300

    2022-12-31

--- src/test/java/day06_/C03_ClassWork.java
+++ src/test/java/day06_/C03_ClassWork.java
@@ -1,12 +1,17 @@
 package day06_;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C03_ClassWork {
 
@@ -35,8 +40,30 @@ public class C03_ClassWork {
 
 
     @Test
+    public void test01() throws InterruptedException {
+        driver.findElement(By.xpath("(//*[.='Phones & PDAs'])[1]")).click();
+        List<WebElement> phoneList = driver.findElements(By.xpath("//h4"));
+        for (WebElement w : phoneList) {
+            System.out.println(w.getText());
+        }
 
-    public void test01(){
+        List<WebElement> addToCartList = driver.findElements(By.xpath("//*[.='Add to Cart']"));
+        for (WebElement w : addToCartList) {
+            w.click();
+        }
+        driver.findElement(By.xpath("//*[@class='btn btn-inverse btn-block btn-lg dropdown-toggle']")).click();
+        List<WebElement> shopCartList = driver.findElements(By.xpath("//*[@class='text-left']"));
+        for (WebElement w : addToCartList) {
+            System.out.println(w.getText());
+        }
+
+        for (WebElement w : phoneList) {
+            for (WebElement t: shopCartList) {
+                Assert.assertEquals(w.getText(),t.getText());
+            }
+        }
+
+
 
 
     }
