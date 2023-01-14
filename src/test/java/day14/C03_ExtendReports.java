f7cd04de3646e1a1a666662630150029b57f7ae7
Yunus Poyraz <yunuspoyraz710@gmail.com>
2023-01-14 23:53:47 +0300

    2023-01-14

--- src/test/java/day14/C03_ExtendReports.java
+++ src/test/java/day14/C03_ExtendReports.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
 
 import java.text.SimpleDateFormat;
@@ -12,13 +13,10 @@ import java.util.Date;
 public class C03_ExtendReports extends TestBase {
 
 
-//
-
-
         //   HATIRLAMANIZ GEREKEN 3 CLASS
-        protected static ExtentReports extentReports;
-        protected static ExtentHtmlReporter extentHtmlReporter;
-        protected static ExtentTest extentTest;
+        protected static ExtentReports extentReports; // sablonu olusturur.
+        protected static ExtentHtmlReporter extentHtmlReporter; // sablonu projesye ekler.
+        protected static ExtentTest extentTest; // raporlama islemi yapar. Logları rapora yazdırır.
 
         @Test
         public void extentReportsTest() {
@@ -50,7 +48,57 @@ public class C03_ExtendReports extends TestBase {
 //        Raporu projeme ekliyorum
             extentReports.attachReporter(extentHtmlReporter);
 
+            extentTest = extentReports.createTest("Extent Report Login Test", "Smoke Test Report");
 
+//       TUM AYARLAR BITTI. EXTENT TEST OBJESİ İLE LOGLAMA(RAPORA YAZDIRMA) ISLEMI YAPABILIRIM.
+        extentTest.pass("Kullanıcı ana sayfaya gider");
+            driver.get("https://www.techproeducation.com");
+
+//      LMS SAYFASINA GIDELIM
+        extentTest.pass("Kullanıcı LMS sayfasına gider");
+        driver.findElement(By.linkText("LMS LOGIN")).click();
+
+//      TEST BITTI
+        extentTest.pass("TEST BASARIYLA GECTI");
+
+//      RAPORU GOSTER. RAPORUN OLUSMASI ICIN BU ADIM ZORUNLUDUR.
+        extentReports.flush(); // raporu gösterir ve kapatır.
 
         }
+
 }
+
+/*
+****************************************INRERVIEWS QUESTIONS************************************************************
+
+Raporlaramalari nasıl yaparsın?
+Manual testing yaparken elle ekran goruntusu alip work documanina eklerim. Sonra bu raporları JIRA ya user storye yüklerim
+Otomasyonda ise Extent Reports ile html raporlarını cikartip
+Extent Reports raporlari değiştirilebilir, özel proje bilgiler eklenebilir raporlardır
+Projemde ki raporlama sistemi, test case fail ettiginde otomatik olarak ekran goruntusunu alacak sekilde ayarladim
+
+Extent Reports kullanmanın avantajları nelerdir? Neden Extent Reports i tercih ettiniz?
+Open source- Ücretsiz
+Farkli browserlar yada islerim sistemleriyle calisabilir
+Junit, testng, cucumber gibi farklı test otomasyon frameworkleriyle entegre olabilir
+Her bir test adimini log(rapora yazdirma) yapabiliriz
+Customizable-Editlenebilir
+
+Extent reports ile raporlama islemi nasil gerceklesir? Bilgi verirmisiniz?
+Oncelikle maven projeleri icin Extent reports dependency oluşturmamız gerekmektedir
+Reusable classimda extent reports ile alakalı reusable metotlarım var. Bu metotlar yardımıyla raporlarım oluşuyor.
+3 onemli class vardır:
+Extent reports
+Extent Html Reporter.  ====>>>>>>> are user for generation the reports
+Extent Tess
+Bu sınıflar yardımıyla, platform, browser, environment, tester, takim bilgileri gibi özel proje bilgileri ekleyebilirim.
+Ayrıca extent test objesi kullanarak pass, fail, skip gibi loglamalar yapabilirim.
+
+Dependency lerini nerden alıyorsun?
+Şirkette internal bir repository miz var. Bu repodan ihtiyacım olacak dependencyi alıp kullanırım.
+
+Dependency sirket reposunda yoksa ne yapılır?
+Istedigim dependency icin istekde bulunuyorum. Managerlerin onayından sonra eklenen dependencyleri kullanıyorum.
+ */
+
+
---
+++ src/test/java/day15/C02_WebTables.java
@@ -0,0 +1,43 @@
+package day15;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class C02_WebTables extends TestBase {
+
+    @Test
+    public void WebTablesTest(){
+
+        // https://the-internet.herokuapp.com/tables.
+        driver.get("http://the-internet.herokuapp.com/tables");
+
+        // Task 1 : Table1’i print edin.
+        String table1 = driver.findElement(By.xpath("//table[@id='table1']")).getText();
+        System.out.println(table1);
+
+        // Task 2 : 3. Row’ datalarını print edin.
+        List <WebElement> row3Elements = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr[3]//td"));
+        row3Elements.stream().forEach(t-> System.out.println(t.getText()));
+
+        // Task 3 : Son row daki dataları print edin.
+        List<WebElement> sonSatir = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr[last()]//td"));
+        sonSatir.forEach(veri-> System.out.println(veri.getText()));
+
+        // Task 4 : 5. Column datalarini print edin.
+        List<WebElement> besinciSutunTumVeriler = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr//td[5]"));
+        besinciSutunTumVeriler.forEach(t-> System.out.println(t.getText()));
+
+        // Task 5 : Iki parametreli bir Java metot oluşturalım: printData
+        // Parameter 1 = row numarasi
+        // Parameter 2 = column numarasi
+        // printData(2,3);  => 2nd row, 3rd column daki datayı print etsin
+
+
+
+    }
+
+}
